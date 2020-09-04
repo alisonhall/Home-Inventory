bae46b9fd2bf831b3d58e61920645790fb2521ad
Alison Hall <ahall7@sapient.com>
2020-09-04 15:48:32 -0400

    Properly handle the deleting of items

--- src/ViewItem.tsx
+++ src/ViewItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
@@ -34,6 +34,7 @@ function ViewItem(props: ViewItemProps) {
     const [item, setItem] = useState<Item | null>(null);
     const [containingItemIds, setContainingItemIds] = useState<string[]>([]);
     const [withinItem, setWithinItem] = useState<Item | null>(null);
+    const [locationIds, setLocationIds] = useState<string[]>([]);
     useEffect(() => {
         itemId && itemsRef.child(itemId).on('value', (snapshot) => {
             let item = snapshot.val();
@@ -52,10 +53,72 @@ function ViewItem(props: ViewItemProps) {
             setWithinItem(null);
         }
     }, [item]);
+    useEffect(() => {
+        locationsRef.on('value', (snapshot) => {
+            let item = snapshot.val();
+            setLocationIds(Object.values(item));
+        });
+    }, []);
+
+    let history = useHistory();
 
     if (!item) {
         return null;
     }
+
+    const deleteItem = (e: React.FormEvent<EventTarget>) => {
+        try {
+            e.preventDefault();
+
+            console.log({ item, withinItem, containingItemIds });
+            if (containingItemIds) {
+                alert('Unable to delete because this item contains other items within it. Delete or move those items within before trying again.')
+            } else {
+                let parentContainingIds = withinItem && withinItem.containing && Object.values(withinItem.containing);
+
+                if (parentContainingIds) {
+                    const itemIndexInParent = parentContainingIds && parentContainingIds.indexOf(itemId);
+
+                    console.log({ itemIndexInParent, parentContainingIds });
+
+                    if (typeof itemIndexInParent === 'number' && itemIndexInParent >= 0) {
+                        parentContainingIds.splice(itemIndexInParent, 1);
+
+                        const updatedParentItem = {
+                            ...withinItem,
+                            containing: parentContainingIds
+                        };
+
+                        console.log({updatedParentItem});
+            
+                        const updates: any = {};
+                        updates[`/items/${withinItem && withinItem.id}`] = updatedParentItem;
+                        databaseRef.update(updates);
+                    }
+
+                    itemsRef.child(itemId).remove();
+
+                    return history.push(`/view/${withinItem && withinItem.id}`);
+                } else {
+                    const itemIndexInLocations = locationIds && locationIds.indexOf(itemId);
+                    const updatedLocations = [...locationIds];
+                    if (itemIndexInLocations && itemIndexInLocations >= 0) {
+                        updatedLocations.splice(itemIndexInLocations, 1);
+
+                        const updates: any = {};
+                        updates[`/locations`] = [...updatedLocations];
+                        databaseRef.update(updates);
+                    }
+
+                    itemsRef.child(itemId).remove();
+
+                    return history.push(`/`);
+                }
+            }
+        } catch (error) {
+            console.error('ERROR in ViewItem.jsx, deleteItem function', error);
+        }
+    };
 
     return (
         <>
@@ -74,10 +137,11 @@ function ViewItem(props: ViewItemProps) {
                 </Link>
             </IconButton>
             <IconButton>
-            <Link aria-label="edit" to={`/edit/${item.id}`}>
-                <EditIcon fontSize="small" />
-            </Link>
+                <Link aria-label="edit" to={`/edit/${item.id}`}>
+                    <EditIcon fontSize="small" />
+                </Link>
             </IconButton>
+            <IconButton aria-label="delete" onClick={deleteItem}>
                 <DeleteIcon fontSize="small" />
             </IconButton>
             <Divider />
