e34fd9d7719adc50bf15bd842701e811fd50a170
Alison Hall <ahall7@sapient.com>
2020-09-04 14:20:15 -0400

    Use env variables for the firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,24 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_APP_ID,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_DATABASE_URL,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET
+} = process.env;
+
 const config = {
-    apiKey: "XXXX",
-    authDomain: "XXXX",
-    databaseURL: "XXXX",
-    projectId: "XXXX",
-    storageBucket: "XXXX",
-    messagingSenderId: "XXXX",
-    appId: "XXXX",
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(config);
