a041f4d856891d249dfdb632b186af6b828bdb15
Alison Hall <ahall7@sapient.com>
2020-09-05 09:14:51 -0400

    Improve heading styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import { AppBar, Toolbar, Container, Switch as ToggleSwitch, Button } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import { signIn, signOut, checkLoggedInStatus } from './firebase';
 import Home from './Home';
@@ -14,6 +15,17 @@ import EditForm from './EditForm';
 import ViewItem from './ViewItem';
 import './App.scss';
 import Loader from './Loader';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7'
+    },
+    secondary: {
+      main: '#00b0ff'
+    }
+  }
+});
 
 function App() {
   const [showJSON, setShowJSON] = useState(false);
@@ -48,34 +60,39 @@ function App() {
 
   return (
     <Router>
-      <div className="app">
-        <AppBar position="static">
-          <Toolbar>
-            <div className="logo">
-              <Link to="/">Home Inventory</Link>
-            </div>
-            <label>
-              Show RAW data
-              <ToggleSwitch
-                checked={showJSON}
-                onChange={e => setShowJSON(!showJSON)}
-                name="toggleJSON"
-                color="primary"
-                inputProps={{ 'aria-label': 'Toggle JSON' }}
-              />
-            </label>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <AppBar position="static">
+            <Toolbar>
+              <div className="logo">
+                <Link to="/">Home Inventory</Link>
+              </div>
+              <div className="toggle-switch">
+                <label htmlFor="toggleJSON">
+                  Show Data
+                </label>
+                <ToggleSwitch
+                  checked={showJSON}
+                  onChange={e => setShowJSON(!showJSON)}
+                  name="toggleJSON"
+                  color="secondary"
+                  inputProps={{ 'aria-label': 'Toggle JSON' }}
+                />
+              </div>
+              <span className="text-divider">|</span>
+              {typeof isLoggedIn !== 'undefined' && (isLoggedIn
+                ? <Button color="inherit" onClick={signOut}>Logout</Button>
+                : <Button color="inherit" onClick={signIn}>Login</Button>)}
+            </Toolbar>
+          </AppBar>
+          <Container className="container" maxWidth="sm">
+            {typeof isLoggedIn === 'undefined' && <Loader />}
             {typeof isLoggedIn !== 'undefined' && (isLoggedIn
-              ? <Button color="inherit" onClick={signOut}>Logout</Button>
-              : <Button color="inherit" onClick={signIn}>Login</Button>)}
-          </Toolbar>
-        </AppBar>
-        <Container className="container" maxWidth="sm">
-          {typeof isLoggedIn === 'undefined' && <Loader />}
-          {typeof isLoggedIn !== 'undefined' && (isLoggedIn
-            ? routes
-            : <p>You must login to use this app.</p>)}
-        </Container>
-      </div>
+              ? routes
+              : <p>You must login to use this app.</p>)}
+          </Container>
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
